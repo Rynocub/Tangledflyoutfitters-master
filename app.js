4c1ecab3a9e46405ea2250b4e624758f41fbc061
rynocub <silaspiranha@gmail.com>
2022-12-12 16:27:35 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+
+// $('.option').on('click',function(){
+//     $('.option').removeClass('active');
+//     $(this).addClass('active');
+//  })
+ $(document).ready(function(){
+    $('.option').on('click',function(){
+        $('.option').removeClass('active');
+        $(this).addClass('active');
+     })
+ })
